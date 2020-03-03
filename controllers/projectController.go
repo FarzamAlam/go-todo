f960b4f23402b33bfe23086d8b3343c8d01e62fe
FarzamAlam <farzamcse@gmail.com>
2020-03-03 17:10:23 +0530

    Added a controller to add and get all the projects

---
+++ controllers/projectController.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/farzamalam/go-todo/utils"
+
+	"github.com/farzamalam/go-todo/models"
+)
+
+var CreateProject = func(w http.ResponseWriter, r *http.Request) {
+	project := &models.Project{}
+
+	err := json.NewDecoder(r.Body).Decode(project)
+	if err != nil {
+		utils.RespondError(w, http.StatusBadRequest, "Error in the request body.")
+	}
+	err = project.AddProject()
+	if err != nil {
+		log.Fatal("Error in CreateProject : ", err)
+		utils.RespondError(w, http.StatusInternalServerError, "Error while adding project")
+	}
+	utils.Respond(w, http.StatusOK, project)
+}
+
+var GetAllProject = func(w http.ResponseWriter, r *http.Request) {
+	projects := models.GetAllProjects()
+	utils.Respond(w, http.StatusOK, projects)
+}
