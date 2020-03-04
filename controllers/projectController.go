9b2779e3740a311f5349f9911f449ba170c51cbc
FarzamAlam <farzamcse@gmail.com>
2020-03-04 13:19:16 +0530

    Added condition to check the project first and iff the project is found then query task using project.

--- controllers/projectController.go
+++ controllers/projectController.go
@@ -16,11 +16,13 @@ var CreateProject = func(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(project)
 	if err != nil {
 		utils.RespondError(w, http.StatusBadRequest, "Error in the request body.")
+		return
 	}
 	err = project.AddProject()
 	if err != nil {
-		log.Fatal("Error in CreateProject : ", err)
+		log.Println("Error in CreateProject : ", err)
 		utils.RespondError(w, http.StatusInternalServerError, "Error while adding project")
+		return
 	}
 	utils.Respond(w, http.StatusOK, project)
 }
--- controllers/taskController.go
+++ controllers/taskController.go
@@ -2,9 +2,11 @@ package controllers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/farzamalam/go-todo/utils"
+	"github.com/gorilla/mux"
 
 	"github.com/farzamalam/go-todo/models"
 )
@@ -23,6 +25,28 @@ var CreateTask = func(w http.ResponseWriter, r *http.Request) {
 }
 
 var GetAllTask = func(w http.ResponseWriter, r *http.Request) {
-	tasks := models.GetAllTasks()
+	//To do for particular project.
+	params := mux.Vars(r)
+	projectTitle := params["title"]
+	if projectTitle == "" {
+		utils.RespondError(w, http.StatusBadRequest, "Title of the project is empty.")
+	}
+	project, err, found := models.GetProject(projectTitle)
+	log.Println("found : ", found)
+	if err != nil {
+		utils.RespondError(w, http.StatusInternalServerError, "Error while getting project from title.")
+		return
+	}
+	if !found {
+		log.Println("Inside project not found")
+		utils.RespondError(w, http.StatusBadRequest, "Project not found.")
+		return
+	}
+	log.Println("After the RespondError")
+	tasks, err := models.GetAllTasks(project)
+	if err != nil {
+		utils.RespondError(w, http.StatusInternalServerError, "Error in GetAllTasks.")
+		return
+	}
 	utils.Respond(w, http.StatusOK, tasks)
 }
